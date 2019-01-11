2b654df513e91c416bf252116f8d01454b024401
rchow1944 <rchow2@buffalo.edu>
2019-01-11 17:56:47 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    possible_anagrams.select {|word| self.word.sum == word.sum}
+  end
+end
