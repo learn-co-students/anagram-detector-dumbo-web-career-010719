0cae0d5b0a2d1f9ef4fcce455790f0d7a6867725
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-01-12 18:28:43 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(phase)
+        @phase=phase
+    end
+
+    def phase
+        @phase
+    end
+
+    def match(array)
+        array.map.find_all {|word| word.split("").sort.join==self.phase.split("").sort.join}
+    end
+
+end
