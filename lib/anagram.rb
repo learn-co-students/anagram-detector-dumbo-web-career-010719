13acaab160c7c0563275820492a1fd4b93bf2b33
Norbertlm <noobertlm@gmail.com>
2019-01-12 13:10:53 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word, :matches
+
+    def initialize(word)
+        @word = word
+        @matches = []
+        @word_hash = Hash.new(0)
+        word.split("").each {|char| @word_hash[char] += 1}
+    end
+
+    def match(strings)
+        strings.each {|string| 
+        
+        if counter(string) == @word_hash
+            @matches << string
+        end
+        }
+        @matches
+    end
+
+    def counter(string)
+        count = Hash.new(0)
+        string.split("").each {|char| count[char] += 1}
+        count
+    end
+
+end
