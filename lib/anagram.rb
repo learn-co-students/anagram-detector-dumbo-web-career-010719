985c00c3d50350fc367074194c2bc80f9ec9e0c7
Jesus <jesus.jg.medina@gmail.com>
2019-01-14 17:01:25 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  def initialize(name)
+    @word = name
+  end
+  def match(arr)
+    arr.select do |word|
+      test = true
+      word.split("").each do |letter|
+        if !@word.include?(letter) || @word.count(letter) != word.count(letter)
+          test = false
+        end
+      end
+      test
+  end
+
+  end
+
+
+end
