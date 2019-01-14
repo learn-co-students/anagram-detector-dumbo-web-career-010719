981ecfff8e1a8a26db877c33a0d0980623c53d70
Jun Haeng Lee <johnhaeng@gmail.com>
2019-01-14 06:15:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    anag = %w(words)
+    anag_words = []
+    anag.each do |word|
+      anag_words.ush(word.reverse)
+    end
+    anag_words
+  end
+  
+end
