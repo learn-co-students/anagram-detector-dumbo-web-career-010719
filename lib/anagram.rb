637bc84daf58eeca4dc56e4b5b69fdc353982c25
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-01-14 22:19:10 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word.split("").sort.join("")
+  end
+
+  def match(arr)
+    arr.select{ |elem| elem.split("").sort.join("") == @word}
+  end
+
+
+end
